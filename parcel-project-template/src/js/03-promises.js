453c0fb99ad83f4329541f6bc9cbbb3f2e8c8979
TaraskiN85 <taras12304@i.ua>
2023-10-30 17:55:57 +0200

    add folder

--- parcel-project-template/src/js/03-promises.js
+++ parcel-project-template/src/js/03-promises.js
@@ -1,3 +1,6 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const createBtn = document.querySelector;
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
