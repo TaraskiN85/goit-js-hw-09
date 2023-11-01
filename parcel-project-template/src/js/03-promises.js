614ae54a0f680c808657dd2ecdcea915a13435a7
TaraskiN85 <taras12304@i.ua>
2023-11-01 17:09:49 +0200

    complete task 3

--- parcel-project-template/src/js/03-promises.js
+++ parcel-project-template/src/js/03-promises.js
@@ -1,11 +1,61 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const createBtn = document.querySelector;
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+const createBtn = document.querySelector('.btn');
+
+const [delay, step, amount, ...rest] = [...form.elements];
+
+const promisesData = {
+  delay,
+  step,
+  amount,
+};
+
+const createPromise = (position, delay) => {
+  const promise = new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    const promiseObj = {
+      promisePosition: position,
+      promiseDelay: delay,
+    };
+    setTimeout(() => {
+      if (shouldResolve) {
+        res(promiseObj);
+      } else {
+        rej(promiseObj);
+      }
+    }, delay);
+  });
+
+  return promise;
+};
+
+const createPromises = event => {
+  event.preventDefault();
+  for (let i = 0; i < promisesData.amount; i++) {
+    createPromise(i + 1, promisesData.delay + i * promisesData.step)
+      .then(({ promisePosition, promiseDelay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${promisePosition} in ${promiseDelay}ms`,
+          {
+            timeout: 3000,
+          }
+        );
+      })
+      .catch(({ promisePosition, promiseDelay }) => {
+        Notify.failure(
+          `❌ Rejected promise ${promisePosition} in ${promiseDelay}ms`,
+          {
+            timeout: 3000,
+          }
+        );
+      });
   }
-}
+};
+
+const addData = event => {
+  promisesData[event.target.name] = +event.target.value;
+};
+
+form.addEventListener('input', addData);
+createBtn.addEventListener('click', createPromises);
